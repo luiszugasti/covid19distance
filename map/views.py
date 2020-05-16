ab2d4f9fed1eb171c4c29f66ea0f2e36793f3430
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2020-05-16 02:25:41 -0400

    Added view to testmap

--- map/views.py
+++ map/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, './covid19distance/index.html')
+
+
+def testmap(request):
+    return render(request, './covid19distance/testmap.html')
