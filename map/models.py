8b0df5f4c18b96cae779ba36c6bf692c0bfe0029
Xiang Weng <xiang.weng3@gmail.com>
2020-05-16 18:48:08 -0400

    made edits to model please migrate

--- map/models.py
+++ map/models.py
@@ -2,14 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
-    WEEK_DAYS = [('Mon', 'Monday'),
-                 ('Tue', 'Tuesday'),
-                 ('Wed', 'Wednesday'),
-                 ('Thu', 'Thusday'),
-                 ('Fri', 'Friday'),
-                 ('Sat', 'Saturday'),
-                 ('Sun', 'Sunday')]
     location_name = models.CharField(max_length=100)
-    location_access_day = models.CharField(max_length=9, choices=WEEK_DAYS)
-    location_access_time = models.TimeField()
+    location_access_day = models.CharField(max_length=9)
+    location_access_time = models.IntegerField()
     location_population = models.IntegerField(null=True)
