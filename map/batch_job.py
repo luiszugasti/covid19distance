ed4dee4cfcd603dec42dcef020b4eba164237a74
Luis Zugasti <luis.zugasti@outlook.com>
2020-05-16 18:28:22 -0400

    Update batch_job.py to pick up parks data

--- map/batch_job.py
+++ map/batch_job.py
@@ -1,4 +1,10 @@
 import googlemaps
+import time
+
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
 
 # create the gmaps object
 key = 'AIzaSyAa9kNiGDlJqiOk9iudMkoEOpND4ZKcsFI'
@@ -10,17 +16,37 @@ language = "en-US"
 region = "CA"
 radius = 2000
 
-tst = gmaps.places(
-    "restaurant",
+# get the initial placement...
+test = gmaps.places(
+    type,
     location=location,
     radius=radius,
     # region=region,
     language=language,
-    min_price=0,
-    max_price=4,
-    open_now=True,
+    # min_price=0,
+    # max_price=4,
+    # open_now=True,
     type=type,
 )
 
-print("yay")
+totals = test['results']
 
+# get the subsequent entries for test by iterating thru the places object, sending the new value of "next_page_token".
+for i in range(0, 10):
+    time.sleep(5)
+    test = gmaps.places(
+        type,
+        location=location,
+        radius=radius,
+        language=language,
+        type=type,
+        page_token=test['next_page_token']
+    )
+    totals.extend(test['results'])
+    try:
+        next_page_token = test['next_page_token']
+    except KeyError:
+        break
+
+
+print("yay")
