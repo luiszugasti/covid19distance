a408e99ec4fd4df085cacdb1a00b4359fca61810
Xiang Weng <xiang.weng3@gmail.com>
2020-05-15 22:10:46 -0400

    created location model

--- map/models.py
+++ map/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    WEEK_DAYS = ['Monday','Tuesday','Wednesday','Thusday','Friday','Saturday','Sunday']
+    location_name = models.CharField(max_length = 100)
+    location_access_day = models.CharField(max_length=9, choices=WEEK_DAYS)
+    location_access_time = models.TimeField()
+    location_population = models.IntegerField(null=True)
