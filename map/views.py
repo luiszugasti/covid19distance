c9a9e0698a36295c9ac6b98f6810ba86afdadd2a
Xiang Weng <xiang.weng3@gmail.com>
2020-05-16 14:53:38 -0400

    make Location accessable to index page as locations

--- map/views.py
+++ map/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Location
 
 def index(request):
-    return render(request, './covid19distance/index.html')
+    locations = Location.objects.all()
+    return render(request, './covid19distance/index.html',{
+        'locations':locations
+    })
