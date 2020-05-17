9eea2d3b25fd703e528d36dac60402e0916d5751
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2020-05-17 09:05:57 -0400

    Added function to display name in admin console

--- map/models.py
+++ map/models.py
@@ -7,4 +7,6 @@ class Location(models.Model):
     location_type = models.CharField(max_length=20,default = 'park')
     location_access_time = models.IntegerField()
     location_population = models.IntegerField(null=True)
-    
+
+    def __str__(self):
+        return self.location_name
