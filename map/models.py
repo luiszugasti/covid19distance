3916d3c963197478c0834522c1b55f6196c683a2
Xiang Weng <xiang.weng3@gmail.com>
2020-05-16 22:02:53 -0400

    update models remember to migrate

--- map/models.py
+++ map/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Location(models.Model):
     location_name = models.CharField(max_length=100)
     location_access_day = models.CharField(max_length=9)
+    location_type = models.CharField(max_length=20,default = 'park')
     location_access_time = models.IntegerField()
     location_population = models.IntegerField(null=True)
-    location_type = models.CharField(max_length=20)
+    
